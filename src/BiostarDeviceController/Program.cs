8b5872905a956caf056459a6fd540e08ca94ecb9
Sean Farmar <s.farmar@gmail.com>
2023-03-08 09:48:44 +0000

    Adding missing files

---
+++ src/BiostarDeviceController/Program.cs
@@ -0,0 +1,18 @@
+﻿using Suprema;
+
+class Program : UnitTest
+{
+    private LogControl lc = new LogControl();
+
+    protected override void runImpl(UInt32 deviceID)
+    {
+        lc.execute(sdkContext, deviceID, true);
+    }
+
+    static void Main(string[] args)
+    {
+        Program program = new Program();
+        program.Title = "Test for log control";
+        program.run();
+    }
+}
