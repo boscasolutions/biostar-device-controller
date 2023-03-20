cea24e288013ff32af8e95c760dc92b733bdfbd3
Sean Farmar <s.farmar@gmail.com>
2023-03-20 20:54:35 +0000

    re factor 1

--- src/gRPC-POC/DeviceController/BoscaClient/Program.cs
+++ src/gRPC-POC/DeviceController/BoscaClient/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+
+// TODO: Get the divece list
+// Subscibe to device events
+// show the events in a console in real time
